fec4f3bef9f47f674e75cf7348de317ead11ebe6
Teamofeyy <timarnd06@gmail.com>
2025-04-12 21:47:15 +0300

    Добавлены карточки отслеживаемых товаров

---
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { TuiAppearance, TuiButton, TuiSurface, TuiTitle } from '@taiga-ui/core';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
+import { MonitoredProduct } from '../../../interfaces/product.interface'
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [
+    TuiAppearance,
+    TuiCardLarge,
+    TuiTitle,
+    TuiButton,
+    DatePipe,
+    TuiSurface,
+    TuiHeader
+  ],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.less'
+})
+export class ProductCardComponent {
+  @Input({ required: true }) product!: MonitoredProduct;
+  @Output() cardClick = new EventEmitter<void>();
+
+  viewHistory() {
+    this.cardClick.emit();
+  }
+
+  openProductUrl() {
+    window.open(this.product.url, '_blank');
+  }
+}
